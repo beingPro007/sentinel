5c8c65991799520d6a757c69cbe7508284ae85e9
beingPro007 <120173992+beingPro007@users.noreply.github.com>
2024-11-11 21:21:39 +0530

    Change the config  of dashboard for correct use of code params

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,24 +2,44 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useCookies } from "react-cookie";
+import { useSearchParams } from "react-router-dom";
 
 export default function Dashboard() {
   const [cookies, setCookie] = useCookies(["access_token", "refresh_token"]);
+  const searchParams = useSearchParams();
+  const code = searchParams.get('code')
 
   useEffect(() => {
     const onLoad = async () => {
       try {
-        const response = await axios.post(
-          "https://api.upstox.com/v2/login/authorization/token"
-        );
+        const axios = require("axios");
+        const qs = require("qs");
+        let data = qs.stringify({
+          code: `${code}`,
+          client_id: process.env.NEXT_PUBLIC_UPSTOX_CLIENT_ID,
+          client_secret: process.env.NEXT_PUBLIC_UPSTOX_CLIENT_SECRET,
+          grant_type: "authorization_code",
+          redirect_uri:
+            "https://sentinel-gautam-ranas-projects.vercel.app/dashboard",
+        });
+        let config = {
+          method: "post",
+          maxBodyLength: Infinity,
+          url: "https://api.upstox.com/v2/login/authorization/token",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Accept: "application/json",
+          },
+          data: data,
+        };
 
-        if (response.status !== 200) {
-          console.error("Error:", response.statusText);
-          return;
-        }
-
-        const data = response.data;
-        console.log(data);
+        axios(config)
+          .then((response) => {
+            console.log(JSON.stringify(response.data));
+          })
+          .catch((error) => {
+            console.log(error);
+          });
 
         let expires = new Date();
         expires.setTime(expires.getTime() + data.expires_in * 1000);
