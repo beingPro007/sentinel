c761e98e2a5351c7e44ff7863b58e7430d1f4860
beingPro007 <120173992+beingPro007@users.noreply.github.com>
2024-11-11 21:22:07 +0530

    Change the page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,28 +1,30 @@
-"use client"
+"use client";
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useCookies } from "react-cookie";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams } from "next/navigation";
+import QueryString from "qs";
 
 export default function Dashboard() {
   const [cookies, setCookie] = useCookies(["access_token", "refresh_token"]);
   const searchParams = useSearchParams();
-  const code = searchParams.get('code')
+  const code = searchParams.get("code");
 
   useEffect(() => {
     const onLoad = async () => {
+      if (!code) return; // Only execute if code is defined
+
       try {
-        const axios = require("axios");
-        const qs = require("qs");
-        let data = qs.stringify({
-          code: `${code}`,
+        const data = QueryString.stringify({
+          code,
           client_id: process.env.NEXT_PUBLIC_UPSTOX_CLIENT_ID,
           client_secret: process.env.NEXT_PUBLIC_UPSTOX_CLIENT_SECRET,
           grant_type: "authorization_code",
           redirect_uri:
             "https://sentinel-gautam-ranas-projects.vercel.app/dashboard",
         });
-        let config = {
+
+        const config = {
           method: "post",
           maxBodyLength: Infinity,
           url: "https://api.upstox.com/v2/login/authorization/token",
@@ -30,20 +32,16 @@ export default function Dashboard() {
             "Content-Type": "application/x-www-form-urlencoded",
             Accept: "application/json",
           },
-          data: data,
+          data,
         };
 
-        axios(config)
-          .then((response) => {
-            console.log(JSON.stringify(response.data));
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        const response = await axios(config);
+        console.log("Response data:", response.data);
 
-        let expires = new Date();
-        expires.setTime(expires.getTime() + data.expires_in * 1000);
-        setCookie("access_token", data.access_token, {
+        // Setting cookies with actual token and expiration
+        const expires = new Date();
+        expires.setTime(expires.getTime() + response.data.expires_in * 1000);
+        setCookie("access_token", response.data.access_token, {
           path: "/",
           expires,
         });
@@ -53,7 +51,7 @@ export default function Dashboard() {
     };
 
     onLoad();
-  }, [setCookie]);
+  }, [setCookie, code]);
 
   return <div>Dashboard</div>;
 }
